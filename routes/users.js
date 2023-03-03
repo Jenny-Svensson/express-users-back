8c18bca4dc0cbab68fbae1482a29dd22b837698f
Jenny Svensson <jenny.svensson@medieinstitutet.se>
2023-03-03 11:45:37 +0100

    :sparkles: add package

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
+const { v4: uuidv4 } = require('uuid');
 var express = require('express');
 var router = express.Router();
 
+let users = [
+  {id: uuidv4(), userName: 'Jenny', password: 12345},
+  {id: uuidv4(), userName: 'Tommy', password: 23456},
+  {id: uuidv4(), userName: 'Oliver', password: 34567},
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(users);
 });
 
 module.exports = router;
